59b4de33fddb06faff381b0bdedd3c532d9ccbb2
Bogomil Filipov <b.filipov@browswave.com>
2020-03-27 14:05:37 +0200

    add init

---
+++ main.py
@@ -0,0 +1,59 @@
+import json
+import requests
+from flask import Flask
+app = Flask('app')
+
+@app.route('/')
+def home_route():
+  headers = {
+    'Referer': 'https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html',
+    'Sec-Fetch-Dest': 'empty',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+  }
+  resp = requests.get('https://services9.arcgis.com/N9p5hsImWXAccRNI/arcgis/rest/services/Nc2JKvYFoAEOFCG5JSI6/FeatureServer/2/query?f=json&where=1%3D1&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*&orderByFields=Confirmed%20desc&resultOffset=0&resultRecordCount=190&cacheHint=true', headers=headers)
+  parsed = json.loads(resp.text)
+  cos = parsed.get('features')
+  html = '''
+  <style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+
+    td, th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+
+    tr:nth-child(even) {
+      background-color: #dddddd;
+    }
+  </style>
+  <table>
+            <tr>
+            <th>Country_Region</th>
+            <th>Confirmed</th>
+            <th>Recovered</th>
+            <th>Deaths</th>
+            <th>Active</th>
+            </tr>'''
+  for co in cos:
+    name = co.get('attributes').get('Country_Region')
+    confirmed =  co.get('attributes').get('Confirmed')
+    Recovered  =  co.get('attributes').get('Recovered')
+    Deaths  =  co.get('attributes').get('Deaths')
+    Active  =  co.get('attributes').get('Active')
+    html = html + f'''<tr>
+                        <td>{name}</td>
+                        <td>{confirmed}</td>
+                        <td>{Recovered}</td>
+                        <td>{Deaths}</td>
+                        <td>{Active}</td>
+                      </tr>'''
+  html = html + '</table>'
+  return html
+
+if __name__=='__main__':
+  app.run(host='0.0.0.0', port=8080)
